32b060e7630bec5f4e19ef3376d1c9a175398c7a
etherian3 <rianziwalker@gmail.com>
2025-04-12 08:27:32 +0700

    feat: add constants for CrowdFunding contract address and ABI

---
+++ Context/constants.js
@@ -0,0 +1,5 @@
+import crowdFund from "./CrowdFund.json";
+
+// CrowdFunding contract address
+export const CrowdFundAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+export const CrowdFundABI = crowdFund.abi;
