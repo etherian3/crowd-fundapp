e6fe24c1d2ba2416eb6e814d5cc8a9defe5ed34b
etheriannn <rianziwalker@gmail.com>
2025-04-19 20:05:00 +0700

    feat: implement Navbar component with wallet connection functionality

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 
-function Navbar() {
-  return <div>Navbar</div>;
-}
+// INTERNAL IMPORT
+import { CrowdFundContext } from "@/Context/CrowdFund";
+import { Logo, Menu } from "../Components/index";
 
-export default Navbar;
+const NavBar = () => {
+  const { currentAccount, connectWallet } = useContext(CrowdFundContext);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const menuList = ["White Paper", "Project", "Donations", "Members"];
+
+  return (
+    <div className="backgroundMain ">
+      <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+        <div className="relative flex items-center justify-between">
+          <div className="flex items-center">
+            <a
+              href="/"
+              aria-label="Company"
+              title="Company"
+              className="inline-flex items-center mr-8"
+            >
+              <Logo color="text-white" />
+              <span className="ml-2 text-xl font-bold tracking-wide text-gray-100 uppercase">
+                Company
+              </span>
+            </a>
+            <ul className="flex items-center hidden space-x-8 lg:flex">
+              {menuList.map((el, i) => (
+                <li key={i + 1}>
+                  <a
+                    href=""
+                    aria-label="Our product"
+                    title="Our product"
+                    className="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:tex-teal-accent-400"
+                  >
+                    {el}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          {!currentAccount && (
+            <ul className="flex items-center hidden space-x-8 lg:flex">
+              <li>
+                <button
+                  onClick={() => connectWallet()}
+                  className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none background"
+                  aria-label="Sign up"
+                  title="Sign up"
+                >
+                  Connect Wallet
+                </button>
+              </li>
+            </ul>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
