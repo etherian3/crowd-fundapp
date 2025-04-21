af7a5e8230f2f29d2b81d64636ab24160e8fe4ff
etheriannn <rianziwalker@gmail.com>
2025-04-21 16:07:12 +0700

    feat: enhance index page with campaign data fetching and display components

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,64 @@
+import React, { useEffect, useContext, useState } from "react";
+
+// INTERNAL IMPORT
+import { CrowdFundContext } from "@/Context/CrowdFund";
+import { Hero, Card, PopUp } from "../Components";
+
 const index = () => {
+  const {
+    titleData,
+    getCampaigns,
+    createCampaign,
+    donate,
+    getUserCampaigns,
+    getDonations,
+  } = useContext(CrowdFundContext);
+
+  const [allcampaign, setAllcampaign] = useState();
+  const [usercampaign, setUsercampaign] = useState();
+
+  useEffect(() => {
+    const getCampaignsData = getCampaigns();
+    const userCampaignsData = getUserCampaigns();
+    return async () => {
+      const allData = await getCampaignsData;
+      const userData = await userCampaignsData;
+      setAllcampaign(allData);
+      setUsercampaign(userData);
+    };
+  }, []);
+
+  // DONATE POPUP MODEL
+  const [openModel, setOpenModel] = useState(false);
+  const [donateCampaign, setDonateCampaign] = useState();
+
+  console.log(donateCampaign);
   return (
-    <div>
-      <h1 className="text-black text-3xl font-bold underline">Niggas</h1>
-    </div>
+    <>
+      <Hero titleData={titleData} createCampaign={createCampaign} />
+
+      <Card
+        title="All Listed Campaign"
+        allcampaign={allcampaign}
+        setOpenModel={setOpenModel}
+        setDonateCampaign={setDonateCampaign}
+      />
+      <Card
+        title="Your Created Campaign"
+        allcampaign={usercampaign}
+        setOpenModel={setOpenModel}
+        setDonateCampaign={setDonateCampaign}
+      />
+
+      {openModel && (
+        <PopUp
+          setOpenModel={setOpenModel}
+          getDonations={getDonations}
+          donate={donate}
+          donateFunction={donate}
+        />
+      )}
+    </>
   );
 };
 
