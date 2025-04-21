abb4c46c4657f9a2210568a6adfa3ed55fa0e27f
etheriannn <rianziwalker@gmail.com>
2025-04-21 15:47:19 +0700

    feat: implement Footer component with product, contact, and useful links sections

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -1,7 +1,62 @@
 import React from "react";
 
 function Footer() {
-  return <div>Footer</div>;
+  const productList = ["Market", "ERC20 Token", "Donation"];
+  const contactList = ["support@cryptoking.com", "info@example", "Contact us"];
+  const usefullLink = ["Home", "About us", "Company Bio"];
+  return (
+    <footer className="text-center text-white backgroundMain lg:text-left">
+      <div className="mx-6 py-10 text-center md:text-left ">
+        <div className="grid-1 grid gap-8 md:grid-cols-2 lg:grid-cols-4">
+          <div className="">
+            <h6 className="mb-4 flex items-center justify-center font-semibold uppercase md:justify-start">
+              Crypto King
+            </h6>
+            <p>
+              Here you can use rows and columns to organize your footer content.
+              Lorem ipsum dolor sit amet.
+            </p>
+          </div>
+          <div className="">
+            <h6 className="mb-4 flex justify-center font-semibold uppercase md:justify-start">
+              Products
+            </h6>
+            {productList.map((el, i) => (
+              <p className="mb-4" key={i + 1}>
+                <a href="#!">{el}</a>
+              </p>
+            ))}
+          </div>
+          <div className="">
+            <h6 className="mb-4 flex justify-center font-semibold uppercase md:justify-start">
+              Useful links
+            </h6>
+            {usefullLink.map((el, i) => (
+              <p className="mb-4" key={i + 1}>
+                <a href="#!">{el}</a>
+              </p>
+            ))}
+          </div>
+          <div className="">
+            <h6 className="mb-4 flex justify-center font-semibold uppercase md:justify-start">
+              Contact
+            </h6>
+            {contactList.map((el, i) => (
+              <p className="mb-4" key={i + 1}>
+                <a href="#!">{el}</a>
+              </p>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="backgroundMain p-6 text-center">
+        <span>2025 Copyright: </span>
+        <a className="font-semibold" href="https://tailwind-elements.com">
+          Crypto King
+        </a>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
