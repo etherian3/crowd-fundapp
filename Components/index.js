720f1fc7352d38a511b3067d9b59e73af072cb1a
etheriannn <rianziwalker@gmail.com>
2025-04-21 21:40:34 +0700

    feat: update index.js to include Hero, Card, and Form components in exports

--- Components/index.js
+++ Components/index.js
@@ -1,6 +1,10 @@
-import Navbar from "./Navbar";
-import Footer from "./Footer";
+import Card from "./Card";
+import Form from "./Form";
 import Logo from "./Logo";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import PopUp from "./PopUp";
 import Menu from "./Menu";
+import Footer from "./Footer";
 
-export { Navbar, Footer, Menu, Logo };
+export { Logo, Card, Form, Hero, Navbar, PopUp, Menu, Footer };
